d11dcbe9b443b82d65fa936e49db7acd4225d084
Jeroen de Bruijn <vidavidorra@gmail.com>
2018-06-01 22:01:19 +0200

    chore: Add empty CLion/cmake project

---
+++ source/bit_twiddling_hacks.c
@@ -0,0 +1,36 @@
+/***********************************************************************************************************************
+ * Begin of file.
+ ***********************************************************************************************************************
+ *
+ * Copyright (c) 2018  Jeroen de Bruijn  <vidavidorra@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************************************************************/
+/** @file
+ * @brief Bit twiddling hacks, also known as 'bit hacks'.
+ *
+ * @author Jeroen de Bruijn
+ * @date 01 June 2018
+ *
+ **********************************************************************************************************************/
+
+/***********************************************************************************************************************
+ * Includes
+ **********************************************************************************************************************/
+#include "bit_twiddling_hacks.h"
+
+/***********************************************************************************************************************
+ * Public functions
+ **********************************************************************************************************************/
+
+
+/* End of file. */
---
+++ source/bit_twiddling_hacks.h
@@ -0,0 +1,47 @@
+/***********************************************************************************************************************
+ * Begin of file.
+ ***********************************************************************************************************************
+ *
+ * Copyright (c) 2018  Jeroen de Bruijn  <vidavidorra@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************************************************************/
+/** @file
+ * @brief Bit twiddling hacks, also known as 'bit hacks'.
+ *
+ * @author Jeroen de Bruijn
+ * @date 01 June 2018
+ *
+ **********************************************************************************************************************/
+
+#pragma once
+
+/***********************************************************************************************************************
+ * Version
+ **********************************************************************************************************************/
+#define BIT_TWIDDLING_HACKS_VERSION_MAJOR 0        /**< Major version number. */
+#define BIT_TWIDDLING_HACKS_VERSION_MINOR 1        /**< Minor version number. */
+#define BIT_TWIDDLING_HACKS_VERSION_PATCH 0        /**< Patch version number. */
+
+/***********************************************************************************************************************
+ * Includes
+ **********************************************************************************************************************/
+
+/***********************************************************************************************************************
+ * Macro definitions
+ **********************************************************************************************************************/
+
+/***********************************************************************************************************************
+ * Public function prototypes
+ **********************************************************************************************************************/
+
+/* End of file. */
